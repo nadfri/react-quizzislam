7ec0ed4e512f0a38ae9526a05bd6ec05f6ff88af
Nader Frigui <nadfri@gmail.com>
2021-05-11 18:48:04 +0200

    test Form Netlify

--- src/Components/FormError/FormError.jsx
+++ src/Components/FormError/FormError.jsx
@@ -5,8 +5,33 @@ import './FormError.scss';
 function FormError(props) {
 	const [displaySuccess, setDisplaySuccess] = useState(false);
 	const [displayForm, setDisplayForm] = useState(true);
+	const [erreur, setErreur] = useState('');
+	const [texte, setTexte] = useState('');
+
+	const encode = (data) => {
+		return Object.keys(data)
+			.map((key) => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
+			.join('&');
+	};
 
 	const handleSubmit = (e) => {
+		fetch('/', {
+			method: 'POST',
+			headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+			body: encode({
+				'form-name': 'Signalement',
+				erreur,
+				texte,
+				question: props.question.question,
+				id: props.question.id,
+			}),
+		})
+			.then((res) => {
+				alert('Success!');
+				console.log(res);
+			})
+			.catch((error) => alert(error));
+
 		e.preventDefault();
 		setDisplayForm(false);
 		setDisplaySuccess(true);
@@ -17,7 +42,7 @@ function FormError(props) {
 			{displayForm && (
 				<fieldset>
 					<legend>Soumettre une erreur</legend>
-					<form name='contact' method='post' onSubmit={handleSubmit} data-netlify='true'>
+					{/* <form name='contact' method='post' onSubmit={handleSubmit} data-netlify='true'>
 						<input type='hidden' name='form-name' value='contact' />
 						<input type='hidden' name='contact' value={props.question.id} />
 						<input type='hidden' name='question' value={props.question.question} />
@@ -44,6 +69,26 @@ function FormError(props) {
 						<button type='button' className='tomato' onClick={props.closeForm}>
 							Annuler
 						</button>
+					</form> */}
+					<form onSubmit={handleSubmit}>
+						<select value={erreur} onChange={(e) => setErreur(e.target.value)} required name="select">
+							<option value=''>Choisir une erreur</option>
+							<option value='reponse'>Réponse fausse</option>
+							<option value='orthographe'>Orthographe</option>
+							<option value='forumation'>Question mal formulée</option>
+							<option value='infomartion'>Information érronée</option>
+							<option value='autre'>Autre</option>
+						</select>
+
+						<textarea
+							name='message'
+							value={texte}
+							onChange={(e) => setTexte(e.target.value)}
+						/>
+
+						<p>
+							<button type='submit'>Send</button>
+						</p>
 					</form>
 				</fieldset>
 			)}
