38443571ce26f2f95b40bf6cf209eb7d8509677c
Nader Frigui <nadfri@gmail.com>
2021-06-02 00:31:30 +0200

    ajout du lien installation sur Iphone

--- src/Components/Settings/Settings.jsx
+++ src/Components/Settings/Settings.jsx
@@ -7,6 +7,7 @@ function Settings() {
 		<div className='Settings'>
 			<Link to='/settings/suggestion'>Une Suggestion</Link>
 			<Link to='/settings/signalement'>Signaler une Erreur</Link>
+			<a href='https://www.youtube.com/watch?v=DASaHIKKD2Q'>Installation Sur Iphone</a>
 			<Link to='/settings/apropos'>À propos</Link>
 			<Link to='/settings/admin' className="admin">Administration</Link>
 		</div>
