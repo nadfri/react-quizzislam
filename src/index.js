42749e54b21fb1a41d609495566e690e1094de36
Nader Frigui <nadfri@gmail.com>
2021-08-15 00:05:35 +0200

    Reset du dernier commit sur l'affichage 100vh

--- src/index.js
+++ src/index.js
@@ -5,12 +5,12 @@ import App from './Components/App/App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 
-/*100vh Sur Mobile*/
 const metas = document.getElementsByTagName('meta');
-// metas[1].content =
-//   'width=device-width, height=' +
-//   window.innerHeight +
-//   ' initial-scale=1.0, maximum-scale=5.0,user-scalable=0';
+
+metas[1].content =
+  'width=device-width, height=' +
+  window.innerHeight +
+  ' initial-scale=1.0, maximum-scale=5.0,user-scalable=0';
 
 ReactDOM.render(
   <React.StrictMode>
