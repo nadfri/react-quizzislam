453c58237efc1583e846a993a494141b573fc946
Nader Frigui <nadfri@gmail.com>
2021-08-21 20:55:33 +0200

    Ajout translateY sur bouton

--- src/index.js
+++ src/index.js
@@ -5,12 +5,13 @@ import App from './Components/App/App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 
+//Empeche le resize de la fenetre à l'ouverture du clavier
 const metas = document.getElementsByTagName('meta');
-
 metas[1].content =
   'width=device-width, height=' +
   window.innerHeight +
   ' initial-scale=1.0, maximum-scale=5.0,user-scalable=0';
+
 
 ReactDOM.render(
   <React.StrictMode>
