451838a5f2eaaea347de4f890bbc52799b636b19
nadfri <nadfri@gmail.com>
2023-05-06 19:02:10 +0200

    Fix: scroll on safari

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -3,41 +3,42 @@ import './NavBar.scss';
 import { NavLink } from 'react-router-dom';
 
 function NavBar() {
-	const refNav = useRef(null);
+  const refNav = useRef(null);
+  const scrollPosRef = useRef(0);
 
-	useEffect(() => {
-		let posY = 10;
-		const handleScroll = () => {
-			window.scrollY > posY
-				? refNav.current.classList.add('hidden')
-				: refNav.current.classList.remove('hidden');
-			posY = window.scrollY;
-		};
+  useEffect(() => {
+    const handleScroll = () => {
+      const isScrollingDown = window.scrollY > scrollPosRef.current;
+      const shouldHideNav = isScrollingDown && window.scrollY > 10;
 
-		document.addEventListener('scroll', handleScroll);
+      refNav.current.classList.toggle('hidden', shouldHideNav);
+      scrollPosRef.current = window.scrollY;
+    };
 
-		return () => document.removeEventListener('scroll', handleScroll);
-	}, []);
+    document.addEventListener('scroll', handleScroll);
 
-	return (
-		<div className='NavBar' ref={refNav}>
-			<NavLink exact to='/'>
-				<i className='fas fa-dice-d6'></i>
-			</NavLink>
-			<NavLink to='/entrainement'>
-				<i className='fas fa-gamepad'></i>
-			</NavLink>
-			<NavLink to='/competition'>
-				<i className='fas fa-trophy'></i>
-			</NavLink>
-			<NavLink exact to='/classement'>
-				<i className='fas fa-medal'></i>
-			</NavLink>
-			<NavLink to='/settings'>
-				<i className='fas fa-tools'></i>
-			</NavLink>
-		</div>
-	);
+    return () => document.removeEventListener('scroll', handleScroll);
+  }, []);
+
+  return (
+    <div className='NavBar' ref={refNav}>
+      <NavLink exact to='/'>
+        <i className='fas fa-dice-d6'></i>
+      </NavLink>
+      <NavLink to='/entrainement'>
+        <i className='fas fa-gamepad'></i>
+      </NavLink>
+      <NavLink to='/competition'>
+        <i className='fas fa-trophy'></i>
+      </NavLink>
+      <NavLink exact to='/classement'>
+        <i className='fas fa-medal'></i>
+      </NavLink>
+      <NavLink to='/settings'>
+        <i className='fas fa-tools'></i>
+      </NavLink>
+    </div>
+  );
 }
 
 export default NavBar;
--- src/Components/ScrollTop/ScrollTop.jsx
+++ src/Components/ScrollTop/ScrollTop.jsx
@@ -1,17 +1,27 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './ScrollTop.scss';
 
 function ScrollTop() {
-	return (
-		<div className='ScrollTop'>
-			<i
-				className='fas fa-arrow-circle-up'
-				onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}></i>
-			<i
-				className='fas fa-arrow-circle-down'
-				onClick={() => window.scrollTo({ top: document.body.scrollHeight, behavior: 'smooth' })}></i>
-		</div>
-	);
+  useEffect(() => {
+    document.body.style.overflowY = 'scroll';
+
+    return () => {
+      document.body.style.overflowY = 'initial';
+    };
+  }, []);
+
+  return (
+    <div className='ScrollTop'>
+      <i
+        className='fas fa-arrow-circle-up'
+        onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}></i>
+      <i
+        className='fas fa-arrow-circle-down'
+        onClick={() =>
+          window.scrollTo({ top: document.body.scrollHeight, behavior: 'smooth' })
+        }></i>
+    </div>
+  );
 }
 
 export default ScrollTop;
