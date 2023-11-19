a8b7d0f85b2efd5a84b81026abb9f4e774ee4faf
nadfri <nadfri@gmail.com>
2023-11-19 13:33:21 +0100

    Fix: replace fa icon by react icon

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -1,6 +1,11 @@
-import React, { useEffect, useRef } from 'react';
 import './NavBar.scss';
+import React, { useEffect, useRef } from 'react';
 import { NavLink } from 'react-router-dom';
+import { MdMosque } from 'react-icons/md';
+import { FaGamepad } from 'react-icons/fa';
+import { FaTrophy } from 'react-icons/fa';
+import { FaRankingStar } from 'react-icons/fa6';
+import { FaTools } from 'react-icons/fa';
 
 function NavBar() {
   const refNav = useRef(null);
@@ -23,19 +28,23 @@ function NavBar() {
   return (
     <div className='NavBar' ref={refNav}>
       <NavLink exact to='/'>
-        <i className='fas fa-dice-d6'></i>
+        <MdMosque className='icon' />
       </NavLink>
+
       <NavLink to='/entrainement'>
-        <i className='fas fa-gamepad'></i>
+        <FaGamepad className='icon' />
       </NavLink>
+
       <NavLink to='/competition'>
-        <i className='fas fa-trophy'></i>
+        <FaTrophy className='icon' />
       </NavLink>
+
       <NavLink exact to='/classement'>
-        <i className='fas fa-medal'></i>
+        <FaRankingStar className='icon' />
       </NavLink>
+
       <NavLink to='/settings'>
-        <i className='fas fa-tools'></i>
+        <FaTools className='icon' />
       </NavLink>
     </div>
   );
