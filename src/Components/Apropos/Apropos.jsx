748ffc3a1eeedb7ad34111697af64ef7460843d8
nadfri <nadfri@gmail.com>
2023-12-09 17:43:07 +0100

    Update: Apropos

--- src/Components/Apropos/Apropos.jsx
+++ src/Components/Apropos/Apropos.jsx
@@ -1,6 +1,13 @@
-import React from 'react';
-import './Apropos.scss';
 import { BsEnvelopeAtFill } from 'react-icons/bs';
+import { FaLinkedin } from 'react-icons/fa';
+import { FaGithub } from 'react-icons/fa';
+import { BsTwitterX } from 'react-icons/bs';
+import { FaFacebook } from 'react-icons/fa';
+import { FaInstagram } from 'react-icons/fa';
+import { FaExternalLinkAlt } from 'react-icons/fa';
+import { FaGift } from 'react-icons/fa6';
+import { IoLogoYoutube } from "react-icons/io";
+import './Apropos.scss';
 
 function Apropos() {
   return (
@@ -10,20 +17,62 @@ function Apropos() {
         <p>QuizzIslam est une application pour apprendre l'Islam de manière ludique.</p>
 
         <p>
-          <b>Abdelhakim Richi</b>, imam de la mosquée de Savigny Le temple est à
-          l'initiative de ce projet.
+          <b>Abdelhakim Richi</b>{' '}
+          <a href='https://twitter.com/ImamRichi' target='_blank' rel='noreferrer'>
+            <BsTwitterX />
+          </a>{' '}
+          <a
+            href='https://www.facebook.com/abdelhakim.richi/'
+            target='_blank'
+            rel='noreferrer'>
+            <FaFacebook />
+          </a>{' '}
+          <a
+            href='https://www.instagram.com/tv/CYNgjxjI2gi/'
+            target='_blank'
+            rel='noreferrer'>
+            <FaInstagram />
+          </a>
+          , ancien imam de la mosquée de Savigny Le temple{' '}
+          <a href='https://acmslt.fr/' target='_blank' rel='noreferrer'>
+            <FaExternalLinkAlt />
+          </a>{' '}
+          est à l'initiative de ce projet.
+        </p>
+
+        <p>
+          Si vous avez aimé cette application, vous pouvez faire un don pour la
+          construction d'un collège musulman{' '}{' '}
+          <a
+            href='https://masjidbox.com/donations/mosquee-de-savigny-le-temple/travaux-de-finition-dagrandissement-de-lecolecollege-de-la-mosquee-de-savigny-le-temple'
+            target='_blank'
+            rel='noreferrer'>
+            <FaGift />
+          </a>{' '}
         </p>
 
         <p>
           Développé par{' '}
           <a href='mailto:nadfri@gmail.com'>
             NadfriJS <BsEnvelopeAtFill className='icon' />
+          </a>{' '}
+          <a
+            href='https://www.linkedin.com/in/nader-frigui-23509025/'
+            target='_blank'
+            rel='noreferrer'>
+            <FaLinkedin />
+          </a>{' '}
+          <a href='https://github.com/nadfri' target='_blank' rel='noreferrer'>
+            <FaGithub />
+          </a>{' '}
+          <a href='https://www.youtube.com/c/nadfrijs' target='_blank' rel='noreferrer'>
+            <IoLogoYoutube />
           </a>
         </p>
 
-        <p>Version 1.0.21</p>
+        <p>Version 1.1.0</p>
 
-        <p>2021</p>
+        <p>2021-2023</p>
       </fieldset>
     </div>
   );
