3df5b9c4e28ad51f5dc57a27cb765d4b6a340446
Nader Frigui <nadfri@gmail.com>
2025-03-01 23:25:31 +0100

    Feat: ramadan icon to Header

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -3,12 +3,15 @@ import './Header.scss';
 import ToggleBtn from './ToggleBtn/ToggleBtn';
 
 function Header() {
-	return (
-		<div className='Header'>
-			<h1>QuizzIslam</h1>
-			<ToggleBtn/>
-		</div>
-	);
+  return (
+    <div className='Header'>
+      <h1>QuizzIslam</h1>
+
+      <img src='images/ramadan.webp' alt='' className='ramadan-img' />
+
+      <ToggleBtn />
+    </div>
+  );
 }
 
 export default Header;
